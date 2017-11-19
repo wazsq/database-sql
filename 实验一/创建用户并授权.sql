8a1c5107b20288959cc0ffc95ea1c3d58cf97cc7
wazsq <1169683890@qq.com>
2017-11-19 18:54:24 +0800

    update

--- 实验一/创建用户并授权.sql
+++ 实验一/创建用户并授权.sql
@@ -1,12 +1,12 @@
 
---创建用户
+--创建用户名为user_naem的用户
 CREATE USER user_naem IDENTIFIED BY "passwd";
 --分配表空间（users）
 DEFAULT TABLESPACE "USERS";
 --分配临时空间
 TEMPORARY TABLESPACE "TEMP";
 
---设置用户权限
+--设置user_naem的权限
 GRANT "DBA" TO user_naem ; --管理员权限
 GRANT "CONNECT" TO user_naem ; --创建绘画权限
 ALTER USER user_naem DEFAULT ROLE "DBA","CONNECT";
